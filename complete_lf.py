bc310f9622926d9065330abd9232d650f174a700
Rajat Saxena <115080161+rajatsaxena314@users.noreply.github.com>
2026-05-03 16:18:01 +0200

    first_upload

---
+++ complete_lf.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+def integrate_langevin_leapfrog_with_potential(
+    x0,
+    v0,
+    n_steps,
+    dt,
+    g,
+    m,
+    D,
+    U,
+    h,
+    seed=None
+):
+    """
+    Integrate a simple Langevin equation including an external potential using a kick-drift-kick scheme.
+    """
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    time = np.zeros(n_steps)
+    x = np.zeros(n_steps)
+    v = np.zeros(n_steps)
+
+    x[0] = x0
+    v[0] = v0
+
+    def grad(U,x,h):
+        return (U(x+h) - U(x-h))/(2*h)
+    
+    for i in range(1, n_steps):
+
+        time[i] = i * dt
+        
+        # Kick: half-step velocity update
+        R1 = np.random.normal(0, 1)
+        v_half = (
+            v[i - 1]
+            + 0.5 * dt * (-g * v[i - 1] / m)
+            + 0.5 * np.sqrt(D * dt) * R1
+            + 0.5 * dt * (-grad(U, x[i-1], h)/m)
+        )
+
+        # Drift: full-step position update
+        x[i] = x[i - 1] + dt * v_half
+
+        # Kick: second half-step velocity update
+        R2 = np.random.normal(0, 1)
+        v[i] = (
+            v_half
+            + 0.5 * dt * (-g * v_half / m)
+            + np.sqrt(D * dt) * R2
+            + 0.5 * dt * (-grad(U, x[i], h)/m)
+        )
+
+    return time, x, v
